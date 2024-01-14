82758357a3ac122a66a39fc647397b219ad9d463
Kamil Głód <kglod@iteratorshq.com>
2024-01-14 21:22:58 +0100

    Added RefreshTokenService

--- src/main/scala/pl/hungry/auth/AuthModule.scala
+++ src/main/scala/pl/hungry/auth/AuthModule.scala
@@ -6,7 +6,7 @@ import doobie.util.transactor.Transactor
 import org.typelevel.log4cats.LoggerFactory
 import pl.hungry.auth.repositories.{AuthRepository, AuthRepositoryDoobie, UserRefreshTokenRepository, UserRefreshTokenRepositoryDoobie}
 import pl.hungry.auth.routers.AuthRouter
-import pl.hungry.auth.services.{AuthService, LoginService, PasswordService}
+import pl.hungry.auth.services.{AuthService, LoginService, PasswordService, RefreshTokenService}
 import pl.hungry.main.AppConfig.JwtConfig
 
 final case class AuthModule(routes: AuthRouter, passwordService: PasswordService)
@@ -17,7 +17,8 @@ object AuthModule {
     val userRefreshTokenRepository: UserRefreshTokenRepository[ConnectionIO] = new UserRefreshTokenRepositoryDoobie
     val authService: AuthService                                             = new AuthService(authRepository, transactor, config)
     val passwordService: PasswordService                                     = new PasswordService
-    val loginService: LoginService = new LoginService(authService, authRepository, userRefreshTokenRepository, passwordService, transactor)
+    val refreshTokenService: RefreshTokenService                             = new RefreshTokenService(userRefreshTokenRepository, transactor)
+    val loginService: LoginService = new LoginService(authService, authRepository, passwordService, refreshTokenService, transactor)
     val authRouter: AuthRouter     = new AuthRouter(authService, loginService)
 
     AuthModule(authRouter, passwordService)
--- src/main/scala/pl/hungry/auth/services/LoginService.scala
+++ src/main/scala/pl/hungry/auth/services/LoginService.scala
@@ -2,13 +2,11 @@ package pl.hungry.auth.services
 
 import cats.data.EitherT
 import cats.effect.IO
-import cats.effect.kernel.Clock
 import doobie.ConnectionIO
 import doobie.implicits._
 import doobie.util.transactor.Transactor
-import org.typelevel.log4cats.{LoggerFactory, SelfAwareStructuredLogger}
 import pl.hungry.auth.domain._
-import pl.hungry.auth.repositories.{AuthRepository, UserRefreshTokenRepository}
+import pl.hungry.auth.repositories.AuthRepository
 import pl.hungry.auth.routers.in.LoginRequest
 import pl.hungry.auth.services.LoginService.LoginError
 import pl.hungry.user.domain._
@@ -17,11 +15,9 @@ import pl.hungry.utils.error.DomainError
 class LoginService(
   authService: AuthService,
   authRepository: AuthRepository[ConnectionIO],
-  userRefreshTokenRepository: UserRefreshTokenRepository[ConnectionIO],
   passwordService: PasswordService,
-  transactor: Transactor[IO]
-)(implicit loggerFactory: LoggerFactory[IO]) {
-  private val logger: SelfAwareStructuredLogger[IO] = loggerFactory.getLogger
+  refreshTokenService: RefreshTokenService,
+  transactor: Transactor[IO]) {
 
   private type ErrorOr[T] = EitherT[IO, LoginError, T]
 
@@ -30,7 +26,7 @@ class LoginService(
       user            <- ensureUserExists(request.email)
       _               <- validateCredentials(request.password, user.passwordHash)
       token           <- generateToken(user)
-      refreshTokenOpt <- tryInsertRefreshToken(user.id, userAgentOpt)
+      refreshTokenOpt <- insertRefreshToken(user.id, userAgentOpt)
     } yield LoginResponse(token, refreshTokenOpt)
 
     effect.value
@@ -45,19 +41,8 @@ class LoginService(
   private def generateToken(user: AuthUser): ErrorOr[JwtToken] =
     EitherT.right(authService.encode(user))
 
-  private def tryInsertRefreshToken(userId: UserId, userAgentOpt: Option[UserAgent]): ErrorOr[Option[RefreshToken]] =
-    userAgentOpt match {
-      case None =>
-        EitherT.right(logger.warn(s"User-Agent header is empty - cannot insert refresh token for user $userId").as(Option.empty[RefreshToken]))
-      case Some(userAgent) =>
-        EitherT.right {
-          Clock[IO].realTimeInstant.flatMap { now =>
-            val refreshToken     = RefreshToken.generate
-            val userRefreshToken = UserRefreshToken.from(userId, refreshToken, userAgent, now)
-            userRefreshTokenRepository.upsert(userRefreshToken).transact(transactor).map(_ => Some(refreshToken))
-          }
-        }
-    }
+  private def insertRefreshToken(userId: UserId, userAgentOpt: Option[UserAgent]): ErrorOr[Option[RefreshToken]] =
+    EitherT.right(refreshTokenService.upsert(userId, userAgentOpt))
 }
 
 object LoginService {
